60d429ce8b4e401094ff22cb0728b75e0720604e
Karagirl <aniemaesther2@gmail.com>
2023-06-18 12:32:51 +0100

    first commit

---
+++ login.js
@@ -0,0 +1,77 @@
+
+  
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDMiwLIRA67yAZaPKGqKvlivQP_JasOG30",
+    authDomain: "learn-auth-2ef8e.firebaseapp.com",
+    projectId: "learn-auth-2ef8e",
+    storageBucket: "learn-auth-2ef8e.appspot.com",
+    messagingSenderId: "170163396211",
+    appId: "1:170163396211:web:61551dd3c488e7f85b5406"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+
+  // //signuup function//
+
+  let signUpButton = document.getElementById("signup");
+  signUpButton.addEventListener("click", (e) =>{
+    e.preventDefault();
+    console.log("cliked");
+
+    var email = document.getElementById("inputEmail");
+    var password = document.getElementById("inputPassword");
+
+    auth 
+    .createUserWithEmailAndPassword(email.value, password.value)
+    .then((userCredential) => {
+      location.reload();
+      alert("user signup up successful");
+
+      var user = userCredential.user;
+      console.log("user, user.email");
+    })
+
+    .catch((error) =>{
+      var errorCode = error.Code;
+      var errorMessage = error.message;
+      console.log("error code, errorCode");
+      console.log("error Message, error message");
+      alert(errorMessage);
+    });
+
+  });
+
+
+ // //SIGNIN FUNCTION//
+
+// let signInButton = document.getElementById("signin");
+// signInButton.addEventListener("click", (e) =>{
+//   e.preventDefault();
+//   console.log("sign in clicked");
+
+//   var email = document.getElementById("inputEmail");
+//   var password = document.getElementById("inputPassword");
+
+//   auth
+//   .signInWithEmailAndPassword(email.value, password.value)
+//   .then((userCredential) =>{
+//     var user = userCredential.user;
+//     console.log("user", user.email);
+//     window.location = "home.html";
+//   })
+
+//   .catch((error) =>{
+//         var errorCode = error.Code;
+//         var errorMessage = error.message;
+//         console.log("error code, errorCode");
+//         console.log("error Message, errorMessage");
+//         alert(errorMessage);
+    
+//       }); 
+
+// });
+
+
---
+++ logout.js
@@ -0,0 +1,37 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMiwLIRA67yAZaPKGqKvlivQP_JasOG30",
+  authDomain: "learn-auth-2ef8e.firebaseapp.com",
+  projectId: "learn-auth-2ef8e",
+  storageBucket: "learn-auth-2ef8e.appspot.com",
+  messagingSenderId: "170163396211",
+  appId: "1:170163396211:web:61551dd3c488e7f85b5406"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+auth.onAuthStateChanged(function (user){
+  if (user){
+    var email = user.email;
+    var user = document.getElementById("user");
+    var text = document.createTextNode("Kara");
+    user.appendChild(text);
+    console.log(user);
+    // console.log(user.name);
+  }
+})
+
+
+//logout/ function/
+
+let signOutButton = document.getElementById("signout");
+signOutButton.addEventListener("click", (e) =>{
+  e.preventDefault();
+  console.log("logout cliked");
+  auth.signOut();
+  alert("Log out");
+  window.location = "index.html";
+});
